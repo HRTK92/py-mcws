28df0fbb3e6b85e384a325a038f5ca02286e2270
はらたく <70054655+HRTK92@users.noreply.github.com>
2021-09-15 11:36:26 +0900

    add install_requires

--- setup.py
+++ setup.py
@@ -1,14 +1,19 @@
 from setuptools import setup, find_packages
- 
+
+def _requires_from_file(filename):
+    return open(filename).read().splitlines()
+
 setup(
     name='py-mcws',    #パッケージ名
     version="0.0.1",
     description="minecraft",
     long_description="",
     author='HRTK92',
+    url="https://github.com/HRTK92/py-mcws",
     license='MIT',
     classifiers=[
         "Development Status :: 1 - Planning"
-    ]
-    ,packages=['py_mcws']   #パッケージのサブフォルダー
+    ],
+    install_requires=_requires_from_file('requirements.txt'),
+    packages=['py_mcws']   #パッケージのサブフォルダー
 )
