473858c8d23743464759f439193395b6616b5c35
はらたく <70054655+HRTK92@users.noreply.github.com>
2024-01-02 06:11:31 +0000

    on_disconnectイベントの追加とPlayerMessageイベントの処理

--- example.py
+++ example.py
@@ -13,9 +13,14 @@ async def on_connect():
     print("接続しました")
     await server.command("say Hello World!")
 
+@server.event
+async def on_disconnect():
+    print("切断しました")
 
 @server.event
 async def on_PlayerMessage(event):
-    print()
+    print(f"{event['body']['sender']}: {event['body']['message']}")
+    if event["body"]["message"] == "こんにちは":
+        await server.command("say こんにちは！")
 
-server.start()
+server.start(host="0.0.0.0", port=19132)
